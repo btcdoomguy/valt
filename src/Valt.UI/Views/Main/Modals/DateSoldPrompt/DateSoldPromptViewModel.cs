f39e8486e368c55ce8a0816e3f238ec3ecad3e94
btcdoomguy <btcdoomguy@protonmail.com>
2026-07-14 13:05:03 -0300

    feat(quick-260714-i1p-01): update DateSoldPromptViewModel for CalendarDatePicker
    - Change DateSold from DateTime to DateTime? (default today)
    - Add PreviousDay, NextDay, and SelectToday RelayCommands
    - Pass nullable DateSold directly to Response record

--- src/Valt.UI/Views/Main/Modals/DateSoldPrompt/DateSoldPromptViewModel.cs
+++ src/Valt.UI/Views/Main/Modals/DateSoldPrompt/DateSoldPromptViewModel.cs
@@ -9,7 +9,27 @@ namespace Valt.UI.Views.Main.Modals.DateSoldPrompt;
 public partial class DateSoldPromptViewModel : ValtModalViewModel
 {
     [ObservableProperty] private string _windowTitle = language.SoldAssetHistory_DateSold_Title;
-    [ObservableProperty] private DateTime _dateSold = DateTime.Now.Date;
+    [ObservableProperty] private DateTime? _dateSold = DateTime.Now.Date;
+
+    [RelayCommand]
+    private void PreviousDay()
+    {
+        if (DateSold.HasValue)
+            DateSold = DateSold.Value.AddDays(-1);
+    }
+
+    [RelayCommand]
+    private void NextDay()
+    {
+        if (DateSold.HasValue)
+            DateSold = DateSold.Value.AddDays(1);
+    }
+
+    [RelayCommand]
+    private void SelectToday()
+    {
+        DateSold = DateTime.Now.Date;
+    }
 
     [RelayCommand]
     private void Ok()
